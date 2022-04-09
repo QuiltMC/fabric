b3a27e48b3207b0de102cbd8ec54c3e805b1385a
LambdAurora <aurora42lambda@gmail.com>
2022-04-09 21:44:14 +0200

    Update fabric-item-groups-v0/src/main/java/net/fabricmc/fabric/impl/item/group/ItemGroupExtensions.java
    Co-authored-by: Emma C. Pointer-Null <emmaffle@modrinth.com>

--- fabric-item-groups-v0/src/main/java/net/fabricmc/fabric/impl/item/group/ItemGroupExtensions.java
+++ fabric-item-groups-v0/src/main/java/net/fabricmc/fabric/impl/item/group/ItemGroupExtensions.java
@@ -1,7 +1,7 @@
 package net.fabricmc.fabric.impl.item.group;
 
 public interface ItemGroupExtensions extends org.quiltmc.qsl.item.group.impl.ItemGroupExtensions {
-    default void faric_expandArray() {
+    default void fabric_expandArray() {
         quilt$expandArray();
     }
 }

97a972f4424b3ffd5fbee2cfb0f5399cf6a3cf9d
LambdAurora <aurora42lambda@gmail.com>
2022-04-10 11:25:40 +0200

    Hopefully satisfy checkstyle once and for all.

--- fabric-item-groups-v0/src/main/java/net/fabricmc/fabric/impl/item/group/ItemGroupExtensions.java
+++ fabric-item-groups-v0/src/main/java/net/fabricmc/fabric/impl/item/group/ItemGroupExtensions.java
@@ -17,7 +17,7 @@
 package net.fabricmc.fabric.impl.item.group;
 
 public interface ItemGroupExtensions extends org.quiltmc.qsl.item.group.impl.ItemGroupExtensions {
-    default void fabric_expandArray() {
-        quilt$expandArray();
-    }
+	default void fabric_expandArray() {
+		this.quilt$expandArray();
+	}
 }

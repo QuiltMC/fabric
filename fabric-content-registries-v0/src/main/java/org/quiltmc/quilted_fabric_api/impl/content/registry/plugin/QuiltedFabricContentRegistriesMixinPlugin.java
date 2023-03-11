00c46a4659a6c2a6813bb9474c6ebf32162f283f
Eli Orona <eliorona@live.com>
2023-03-11 13:08:55 -0800

    Only apply fix outside of dev and if `-Dquilted_fabric_api.disable_rea_fix` is not set or false.

---
+++ fabric-content-registries-v0/src/main/java/org/quiltmc/quilted_fabric_api/impl/content/registry/plugin/QuiltedFabricContentRegistriesMixinPlugin.java
@@ -0,0 +1,45 @@
+package org.quiltmc.quilted_fabric_api.impl.content.registry.plugin;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.quiltmc.loader.api.QuiltLoader;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+public class QuiltedFabricContentRegistriesMixinPlugin implements IMixinConfigPlugin {
+	@Override
+	public void onLoad(String mixinPackage) {
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return !QuiltLoader.isDevelopmentEnvironment() && !Boolean.getBoolean("quilted_fabric_api.disable_rea_fix");
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+	}
+}
